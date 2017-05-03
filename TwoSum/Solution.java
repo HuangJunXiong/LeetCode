2538ca83349953b17a91ad85177bbfd684929568
HuangJunXiong <923383921@qq.com>
2017-05-03 09:52:08 +0800

    update two sum
    150ms----7ms

--- TwoSum/Solution.java
+++ TwoSum/Solution.java
@@ -1,16 +1,29 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i: nums){
-            list.add(i);
-        }
-        for(int i = 0 ;i<nums.length;i++){
-            int res = target-nums[i];
-            int a = 0;
-            if (list.contains(res) && (a = list.lastIndexOf(res))!= i) {
-                return new int[]{i,a};
+//         List<Integer> list = new ArrayList<Integer>();
+//         for(int i: nums){
+//             list.add(i);
+//         }
+//         for(int i = 0 ;i<nums.length;i++){
+//             int res = target-nums[i];
+//             int a = 0;
+//             if (list.contains(res) && (a = list.lastIndexOf(res))!= i) {
+//                 return new int[]{i,a};
+//             }
+//         }
+//         return new int[]{0,0};
+        if(nums==null || nums.length<2)
+            return new int[]{0,0};
+ 
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]), i};
+            }else{
+                map.put(target-nums[i], i);
             }
         }
+ 
         return new int[]{0,0};
     }
 }
